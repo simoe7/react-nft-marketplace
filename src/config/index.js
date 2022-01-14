1d5ce14e0a52131a33752f5d64998a508e10e4b5
simoe7 <boussakssoumohamed@gmail.com>
2022-01-14 19:04:09 +0100

    reset code

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-const config =  {
-    stakingContractAddress:"0x7f2e8910c353EE7bDEd347Ce6F8424dbcC0d6e73",
-    tokenContractAddress :  "0x947f6b19af55af70746a9422b43488228b65af98",
-    nftContractAddress :  "0x0F45819dad63b57f6F0937493F251cD02dc3FCB6",
-    infuraProviderUrl : "https://rinkeby.infura.io/v3/0aaf107ff06442c49f3571b1394b0106",
-    openseaUrl : "https://rinkeby-api.opensea.io/api/v1/assets"
-}  
-
-export default config
